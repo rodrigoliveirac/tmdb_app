d334d10ff56d2b942b1c21caf8c5220d7cd770fc
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-05-24 15:28:43 -0300

    adding shimmer to movie details screen

--- lib/widgets/ImageSection.dart
+++ lib/widgets/ImageSection.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import 'MovieItem.dart';
 
 class ImageSection extends StatelessWidget {
   const ImageSection({super.key, required this.image});
@@ -7,11 +11,25 @@ class ImageSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      image,
-      width: 600,
-      height: 240,
-      fit: BoxFit.cover,
+    return Stack(
+      fit: StackFit.loose,
+      children: [
+        Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+                color: Colors.grey[300],
+                height: 240,
+                width: 600,
+                child: const Center(heightFactor: 240, widthFactor: 600))),
+        FadeInImage.memoryNetwork(
+          placeholder: kTransparentImage,
+          width: 600,
+          height: 240,
+          fit: BoxFit.cover,
+          image: image,
+        )
+      ],
     );
   }
 }
--- lib/widgets/screens/MovieDetailsScreen.dart
+++ lib/widgets/screens/MovieDetailsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:tmdb_app/navigation/arguments/MovieDetailsScreenArgs.dart';
 
 import '../../controller/MovieDetailsController.dart';
@@ -41,12 +42,10 @@ class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
       appBar: AppBar(
         title: Text(titleAppBar),
       ),
-      body: SingleChildScrollView(
-        child: (widget.movieDetailsController.isLoading)
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : Column(
+      body: (widget.movieDetailsController.isLoading)
+          ? const ShimmerMovieDetails()
+          : SingleChildScrollView(
+              child: Column(
                 children: [
                   ImageSection(
                     image: image,
@@ -54,7 +53,7 @@ class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
                   MovieInfoContent(title: title, overview: overview),
                 ],
               ),
-      ),
+            ),
     );
   }
 
@@ -65,4 +64,71 @@ class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
       overview = movie.overview;
     });
   }
+}
+
+class ShimmerMovieDetails extends StatelessWidget {
+  const ShimmerMovieDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+              color: Colors.grey[300],
+              height: 240,
+              width: 600,
+              child: const Center(heightFactor: 240, widthFactor: 600)),
+        ),
+        ShimmerMovieInfoContent()
+      ],
+    ));
+  }
+}
+
+class ShimmerMovieInfoContent extends StatelessWidget {
+  const ShimmerMovieInfoContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[100]!,
+                    child: Container(
+                        color: Colors.grey[300],
+                        height: 16,
+                        width: double.infinity,
+                        child: const Center(heightFactor: 16, widthFactor: double.infinity)),
+                  ),
+                ),
+                Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                      color: Colors.grey[300],
+                      height: 100,
+                      width: double.infinity,
+                      child: const Center(heightFactor: 200, widthFactor: double.infinity)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
