676fceb9492b10d192257f10ee032814a5270f5a
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-05-23 21:00:29 -0300

    create a controller to handle with business logic

---
+++ lib/controller/MoviesController.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tmdb_app/model/MovieItemModel.dart';
+import 'package:tmdb_app/service/ApiService.dart';
+
+class MoviesController with ChangeNotifier {
+
+  late ApiService service = ApiService();
+
+  late bool _isLoading;
+  bool get isLoading => _isLoading;
+
+  List<MovieItemModel> _movies = [];
+  List<MovieItemModel> get movies => _movies;
+
+  Future<void> loadMovies(void Function(List<MovieItemModel>) onResult) async {
+    _isLoading = true;
+    Future<List<MovieItemModel>> list = service.getMovies();
+    _movies = await list;
+    _isLoading = false;
+    onResult(_movies);
+  }
+}
+
