879804345f7eb3ecc4b0c71fdf2b9bbd8f8f133a
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-05-23 21:01:50 -0300

    improving component

--- lib/widgets/MovieItem.dart
+++ lib/widgets/MovieItem.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_app/api_key.dart';
 
 class MovieItem extends StatelessWidget {
   final String img;
   final String title;
   final String description;
 
-  const MovieItem({super.key, required this.img, required this.title, required this.description});
+  const MovieItem(
+      {super.key,
+      required this.img,
+      required this.title,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Card(
-            child: SizedBox(
-                child: Center(child: Text("$title $description"))
-            )));
+    return Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Image.network( img, fit: BoxFit.fill));
   }
 }
-
-
-
-
-
-
