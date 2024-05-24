17943ec7bfb0e83460f67e8ac64e2895bffc6d80
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-05-23 21:02:37 -0300

    fixing correct url_img

--- lib/service/ApiService.dart
+++ lib/service/ApiService.dart
@@ -2,23 +2,54 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:tmdb_app/model/MovieItemModel.dart';
+import 'package:tmdb_app/model/PopularModel.dart';
+
+import '../api_key.dart';
 
 class ApiService {
   String endpoint = 'https://api.themoviedb.org/3/movie/popular';
+  final String imgUrl = 'https://image.tmdb.org/t/p/original/';
 
-  Future<void> getMovies() async {
-
+  Future<List<MovieItemModel>> getMovies() async {
     var client = HttpClient();
     try {
       HttpClientRequest request = await client.getUrl(Uri.parse(endpoint));
-      request.headers.contentType = ContentType('application', 'json', charset: 'utf-8');
+      request.headers.contentType =
+          ContentType('application', 'json', charset: 'utf-8');
       request.headers.add(HttpHeaders.acceptHeader, 'application/json');
-      request.headers.add(HttpHeaders.authorizationHeader, 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTliZTQ5YmU5ZTE2OGI1NjhmYmRiOWIxYzA4NjIzZSIsInN1YiI6IjYyYTdjMzY2MzM2ZTAxMDA1MGRiNTFjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2VCYuerWrhKGkD_72tcodTcDnZP8o6XxW0neb51reZI');
+      request.headers.add(HttpHeaders.authorizationHeader, 'Bearer $API_KEY');
       HttpClientResponse response = await request.close();
+
       final stringData = await response.transform(utf8.decoder).join();
-      print(stringData);
+      final movieMap = jsonDecode(stringData) as Map<String, dynamic>;
+
+      final list = PopularModel.fromJson(movieMap)
+          .results
+          .map((item) =>
+          MovieItemModel(
+              title: item.title,
+              overview: item.overview,
+              img: getUriImg(imgUrl, item.img),
+              adult: item.adult,
+              genreIds: item.genreIds,
+              id: item.id,
+              originalLang: item.originalLang,
+              popularity: item.popularity,
+              posterPath: getUriImg(imgUrl, item.posterPath),
+              releaseDate: item.releaseDate,
+              originalTitle: item.originalTitle,
+              video: item.video,
+              voteCount: item.voteCount,
+              voteAverage: item.voteAverage))
+          .toList();
+      print(list.first.releaseDate);
+      return list;
     } finally {
       client.close();
     }
+  }
+  String getUriImg(String endpoint, String img) {
+    print('$endpoint$img');
+    return '$endpoint$img';
   }
 }
