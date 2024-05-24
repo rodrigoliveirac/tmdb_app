065ec07dc375dbe56a3c062fb1dcd9e6f7b7ae14
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-05-24 14:06:20 -0300

    fetch movie details from api by controller

--- lib/controller/MovieDetailsController.dart
+++ lib/controller/MovieDetailsController.dart
@@ -2,8 +2,34 @@ import '../service/ApiService.dart';
 
 class MovieDetailsController {
   final ApiService service;
+  late bool _isLoading;
+
+  bool get isLoading => _isLoading;
+  late MovieDetailsModel _movieDetailsModel;
+
+  MovieDetailsModel get model => _movieDetailsModel;
+
   MovieDetailsController({required this.service});
-  void loadMovieDetails(String id) {
-    //service.getMovieDetails(id)
+
+  void loadMovieDetails(
+      String id, void Function(MovieDetailsModel) callback) async {
+    _isLoading = true;
+    Future<MovieDetailsModel> movieDetails = service.getMovieDetails(id);
+    _movieDetailsModel = await movieDetails;
+    _isLoading = false;
+    callback(_movieDetailsModel);
   }
+}
+
+class MovieDetailsModel {
+  int id;
+  String image;
+  String title;
+  String overview;
+
+  MovieDetailsModel(
+      {required this.id,
+      required this.title,
+      required this.image,
+      required this.overview});
 }
